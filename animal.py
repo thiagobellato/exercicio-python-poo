566a3135f30b1ed05c6e9b11039b9800607b5da7
Thiago <tfbellato@hotmail.com>
2025-12-29 15:37:10 -0300

    feat: inclusão do exercício do módulo 3 (Herança e Polimorfismo)

---
+++ animal.py
@@ -0,0 +1,20 @@
+class Animal:
+    def falar(self):
+        print("Som genérico")
+
+
+class Cachorro(Animal):
+    def falar(self):
+        print("Au Au")
+
+
+class Gato(Animal):
+    def falar(self):
+        print("Miau")
+
+
+cachorro = Cachorro()
+gato = Gato()
+
+cachorro.falar()
+gato.falar()
